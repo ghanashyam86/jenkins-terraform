2f2eeb2c322fcf7dff9fc23a83c37f32743c5a61
ghana <new@gmail.com>
2025-05-07 16:13:47 +0530

    variable

--- variable.tf
+++ variable.tf
@@ -7,3 +7,14 @@ variable "vpc_cidr_block" {
   type = string
   default = "10.10.0.0/16"
 }
+
+variable "vpc_azs" {
+   type = list(string)
+   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "public_subnets" {
+    type = list(string)
+    default = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+}
+
